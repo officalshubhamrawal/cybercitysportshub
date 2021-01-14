776f9b6d9fa074c9bd4d70cd6731123396254efb
Shubham Rawal <44638774+officalshubhamrawal@users.noreply.github.com>
2021-01-15 01:50:17 +0530

    new

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -381,11 +381,11 @@
 		function googleMapsInit() {
 			var mapOptions = {
 				zoom: 11,
-				center: new google.maps.LatLng(40.6501038, -73.9495823),
-				mapTypeControl: false,
+				center: new google.maps.LatLng(28.466591, 77.033310),
+				mapTypeControl: true,
 				fullscreenControl: false,
 				scalecontrol: false,
-				zoomControl: false,
+				zoomControl: true,
 				streetViewControl: false,
 				rotateControl: false,
 				styles: [{'featureType':'all','elementType':'labels.text.fill','stylers':[{'saturation':36},{'color':'#000000'},{'lightness':40}]},{'featureType':'all','elementType':'labels.text.stroke','stylers':[{'visibility':'on'},{'color':'#000000'},{'lightness':16}]},{'featureType':'all','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'administrative','elementType':'geometry.fill','stylers':[{'color':'#000000'},{'lightness':20}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'color':'#000000'},{'lightness':17},{'weight':1.2}]},{'featureType':'landscape','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':20}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':21}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#000000'},{'lightness':17}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#000000'},{'lightness':29},{'weight':0.2}]},{'featureType':'road.arterial','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':18}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':16}]},{'featureType':'transit','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':19}]},{'featureType':'water','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':17}]}]
@@ -393,7 +393,7 @@
 			mapElement = document.getElementById( 'map' ),
 			map = new google.maps.Map( mapElement, mapOptions ),
 			marker = new google.maps.Marker( {
-				position: new google.maps.LatLng( 40.6401038, -73.9495823 ),
+				position: new google.maps.LatLng( 28.466591, 77.033310 ),
 				map: map,
 				icon: 'assets/img/map-point.png'
 			});
